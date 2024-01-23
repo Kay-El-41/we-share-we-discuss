b48f9e75c6b7e3239f3a74772015294002d223b8
Kay-El-41 <z.khantolino@gmail.com>
2024-01-23 23:11:33 +0630

    Created components and actions for comments

--- app/topics/[slug]/posts/[postId]/page.tsx
+++ app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Link from "next/link";
+import PostShow from "@/src/components/posts/post-show";
+import CommentList from "@/src/components/comments/comment-list";
+import CommentCreateForm from "@/src/components/comments/comment-create-form";
+import paths from "@/src/paths";
 
-const page = () => {
-  return <div>page</div>;
-};
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
 
-export default page;
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"< "}Back to {decodeURI(slug)}
+      </Link>
+      {/* <PostShow /> */}
+      {/* <CommentCreateForm postId={postId} startOpen /> */}
+      {/* <CommentList comments={comments} /> */}
+    </div>
+  );
+}
--- src/actions/create-comment.ts
+++ src/actions/create-comment.ts
@@ -1,5 +1,88 @@
 "use server";
 
-export async function createComment() {
-  // TODO: Revalidate the Show Post Page
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import { auth } from "@/src/auth";
+import { database } from "@/src/database/database";
+import paths from "@/src/paths";
+
+const createCommentSchema = z.object({
+  content: z.string().min(3),
+});
+
+interface CreateCommentFormState {
+  errors: {
+    content?: string[];
+    _form?: string[];
+  };
+  success?: boolean;
+}
+
+export async function createComment(
+  { postId, parentId }: { postId: string; parentId?: string },
+  formState: CreateCommentFormState,
+  formData: FormData
+): Promise<CreateCommentFormState> {
+  const result = createCommentSchema.safeParse({
+    content: formData.get("content"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must sign in to do this."],
+      },
+    };
+  }
+
+  try {
+    await database.comment.create({
+      data: {
+        content: result.data.content,
+        postId: postId,
+        parentId: parentId,
+        //@ts-ignore: next-auth-type-error, we fixed that problem in the auth.ts
+        userId: session.user.id,
+      },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Something went wrong..."],
+        },
+      };
+    }
+  }
+
+  const topic = await database.topic.findFirst({
+    where: { posts: { some: { id: postId } } },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ["Failed to revalidate topic"],
+      },
+    };
+  }
+
+  revalidatePath(paths.postShow(topic.slug, postId));
+  return {
+    errors: {},
+    success: true,
+  };
 }
