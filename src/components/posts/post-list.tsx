74c1a077cfb942fbc2f1f5d4a1ca5c14373eb37a
Kay-El-41 <z.khantolino@gmail.com>
2024-01-24 21:42:29 +0630

    Make the post list component able to fetch data

--- src/components/posts/post-list.tsx
+++ src/components/posts/post-list.tsx
@@ -1,18 +1,23 @@
-import type { Post, User, Topic } from '@prisma/client';
-import Link from 'next/link';
-import paths from '@/paths';
+import Link from "next/link";
+import paths from "@/src/paths";
+import { PostWithData } from "@/src/database/queries/posts";
 
-// TODO: Get list of posts into this component somehow
-export default function PostList() {
+type PostListProps = {
+  fetchData: () => Promise<PostWithData[]>;
+};
+
+export default async function PostList({ fetchData }: PostListProps) {
+  const posts = await fetchData();
+
   const renderedPosts = posts.map((post) => {
     const topicSlug = post.topic.slug;
 
     if (!topicSlug) {
-      throw new Error('Need a slug to link to a post');
+      throw new Error("Need a slug to link to a post");
     }
 
     return (
-      <div key={post.id} className="border rounded p-2">
+      <div key={post.id} className="rounded border p-2">
         <Link href={paths.postShow(topicSlug, post.id)}>
           <h3 className="text-lg font-bold">{post.title}</h3>
           <div className="flex flex-row gap-8">
---
+++ src/database/queries/posts.ts
@@ -0,0 +1,21 @@
+import type { Post } from "@prisma/client";
+import { database } from "@/src/database/database";
+
+export type PostWithData = Post & {
+  topic: { slug: string };
+  user: { name: string | null };
+  _count: { comments: number };
+};
+
+export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
+  return database.post.findMany({
+    where: {
+      topic: { slug },
+    },
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true } },
+      _count: { select: { comments: true } },
+    },
+  });
+}
