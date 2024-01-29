95bab0b828692b00bb7baf106a8c0a319e423e07
Kay-El-41 <z.khantolino@gmail.com>
2024-01-29 22:23:15 +0630

    Added Request Memoization using cache function to deduplicated server calls

--- src/database/queries/comment.ts
+++ src/database/queries/comment.ts
@@ -1,22 +1,24 @@
 import type { Comment } from "@prisma/client";
+import { cache } from "react";
 import { database } from "@/src/database/database";
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentByPostId(
-  postId: string
-): Promise<CommentWithAuthor[]> {
-  return database.comment.findMany({
-    where: { postId },
-    include: {
-      user: {
-        select: {
-          name: true,
-          image: true,
+export const fetchCommentByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    console.log("making a query");
+    return database.comment.findMany({
+      where: { postId },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
         },
       },
-    },
-  });
-}
+    });
+  }
+);
